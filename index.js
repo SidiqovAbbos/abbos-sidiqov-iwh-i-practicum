ae72dd2891b754a00620b0f4f3f65297cf543b26
Abbos Sidiqov <abbosidiqov@outlook.com>
2025-03-10 15:36:13 +0500

    First commit to my Integrating With HubSpot I: Foundations practicum repository

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-const express = require('express');
-const axios = require('axios');
-const app = express();
+const express = require("express");
+const axios = require("axios");
+const dotenv = require("dotenv");
 
-app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
+dotenv.config();
+
+const app = express();
+app.set("view engine", "pug");
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_KEY;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get("/", async (req, res) => {
+  const customObjects = "https://api.hubspot.com/crm/v3/objects/contacts";
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    "Content-Type": "application/json",
+  };
+  console.log(PRIVATE_APP_ACCESS);
+  try {
+    const resp = await axios.get(customObjects, { headers });
+    const data = resp.data.results;
+    console.log(data);
+    res.render("contacts", { title: "Custom Objects | HubSpot APIs", data });
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
@@ -66,6 +84,5 @@ app.post('/update', async (req, res) => {
 });
 */
 
-
 // * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.listen(3000, () => console.log("Listening on http://localhost:3000"));
